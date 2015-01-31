fc4c474e455f55f2d3fba3a690f4e0ffc2538eae
kristian <kbouwser@gmail.com>
2015-01-31 00:54:18 -0500

    content and routing updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,15 +8,15 @@ class PagesController < ApplicationController
   def web_landing_two
   end
 
+  def web_landing_three
+  end
+
   def mobile_landing_one
   end
 
   def mobile_landing_two
   end
 
-  def mixed_landing_one
-  end
-
-  def mixed_landing_two
+  def mobile_landing_three
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   get 'web-landing-one', to: 'pages#web-landing-one'
   get 'web-landing-two', to: 'pages#web-landing-two'
+  get 'web-landing-three', to: 'pages#web-landing-three'
   get 'mobile-landing-one', to: 'pages#mobile-landing-one'
   get 'mobile-landing-two', to: 'pages#mobile-landing-two'
-  get 'mixed-landing-one', to: "pages#mixed-landing-one"
-  get 'mixed-landing-two', to: "pages#mixed-landing-two"
+  get 'mobile-landing-three', to: 'pages#mobile-landing-three'
   
 end
