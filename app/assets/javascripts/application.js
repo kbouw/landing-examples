249cbbc2c47d8c8e0c1d65c0f3edb4c53cbad63b
kristian <kbouwser@gmail.com>
2015-02-01 23:20:07 -0500

    updated content; added pricing and testimonial; implemented jQuery for closing pricing and testimonial sections

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,15 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+    $(".close-pricing").click(function(){
+        event.preventDefault();
+        $(".pricing").hide();
+    });
+
+    $(".close-testimonial").click(function(){
+        event.preventDefault();
+        $(".testimonial").hide();
+    });
+});
